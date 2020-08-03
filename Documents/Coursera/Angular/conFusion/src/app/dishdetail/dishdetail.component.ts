b10b0a805218f55d909d9d89d76fc556daf56c2e
Shubham Abnave <abnaveshubham111@.com>
2020-08-03 14:45:38 +0530

    Single Page Application Part 2

--- Documents/Coursera/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Documents/Coursera/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { ActivatedRoute, Params} from '@angular/router';
+import { Location} from '@angular/common'; 
+import {DishService} from '../services/dish.service';
+
 import {Dish} from '../shared/dish';
 
 
@@ -11,10 +15,18 @@ export class DishdetailComponent implements OnInit {
 
   @Input()
   dish: Dish;
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
+  goBack():void{
+    this.location.back();
+  }
+
   //dish = ;
   //allComments= DISH.comments;
 }
