96ad43aac72f6519f99e3a0471278e6868b3e8fb
Shubham Abnave <abnaveshubham111@.com>
2020-08-09 12:03:05 +0530

    Dialogs

--- Documents/Coursera/Angular/conFusion/src/app/app.module.ts
+++ Documents/Coursera/Angular/conFusion/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { MatListModule} from '@angular/material/list';
 import { MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 
 import { FlexLayoutModule} from '@angular/flex-layout';
@@ -25,6 +27,10 @@ import { PromotionService} from './services/promotion.service';
 import { LeaderService} from './services/leader.service';
 
 import { AppRoutingModule} from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -35,7 +41,8 @@ import { AppRoutingModule} from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -46,8 +53,11 @@ import { AppRoutingModule} from './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
     AppRoutingModule
   ],
+    
+  entryComponents:[ LoginComponent ],
   providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
